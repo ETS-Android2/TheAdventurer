ae34f0a4dedd45a337d1528d162f90aa34826d9f
arvandamme <arhur.van_damme@etu.uca.fr>
2022-02-03 11:46:28 +0100

    Modification total du git

---
+++ app/src/main/java/fr/iut63/a2ddicegameupdate/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.iut63.a2ddicegameupdate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
